dc6d41972dd8b39e003628817b63ccd86e3a75d9
rafael vieira <rafaelgtvc@virtualisurg.com>
2023-07-12 19:50:11 -0300

    Adding cupom

--- photo-gallery/src/app/Ofertas/Cupom.component.ts
+++ photo-gallery/src/app/Ofertas/Cupom.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
+import { NgxQRCodeModule, NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels } from '@techiediaries/ngx-qrcode';
 
 import { IonicModule, ModalController } from '@ionic/angular';
-
 // @NgModule({
 //   imports: [CommonModule, IonicModule],
 //   // selector: 'cupom',
@@ -13,11 +13,14 @@ import { IonicModule, ModalController } from '@ionic/angular';
 @Component({
   selector: 'cupom',
   templateUrl: 'Cupom.component.html',
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, NgxQRCodeModule],
   standalone: true,
 })
 export class Cupom {
   name!: string;
+  elementType = NgxQrcodeElementTypes.URL;
+  correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
+  value = 'https://www.techiediaries.com/';
 
   constructor(private modalCtrl: ModalController) {}
 
